b2b7857ca74ef6d96e2824a58267e6505b9ae50c
danielbasz <danielbassporto@gmail.com>
2022-06-09 16:48:29 -0400

    Test Practice 1

--- app/src/main/java/com/college/quiz1_question/MainActivity.java
+++ app/src/main/java/com/college/quiz1_question/MainActivity.java
@@ -3,7 +3,12 @@ package com.college.quiz1_question;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +20,29 @@ public class MainActivity extends AppCompatActivity {
 
         //counterView is the TextView that displays the value of the counter
         TextView counterView = findViewById(R.id.counterId);
+        Button counterReset = findViewById(R.id.buttonResetId);
 
-        //TODO
-        //Your code here
+        counterView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCounter++;
+                TextView counterText = findViewById(R.id.counterId);
+                counterText.setText(String.valueOf(mCounter));
+                Snackbar snack = Snackbar.make(view, "the counter is now " + mCounter, Snackbar.LENGTH_LONG);
+                snack.show();
+            }
+        });
+
+        counterReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCounter = 0;
+                TextView counterText = findViewById(R.id.counterId);
+                counterText.setText(String.valueOf(mCounter));
+                Snackbar snack = Snackbar.make(view, "the counter is now " + mCounter, Snackbar.LENGTH_LONG);
+                snack.show();
+            }
+        });
 
     }
 }
