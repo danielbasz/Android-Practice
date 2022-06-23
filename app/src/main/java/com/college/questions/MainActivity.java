d04c046826813be250a580872edb0b52fec06ce0
danielbasz <danielbassporto@gmail.com>
2022-06-23 14:28:18 -0400

    Burger Question Practice

--- app/src/main/java/com/college/questions/MainActivity.java
+++ app/src/main/java/com/college/questions/MainActivity.java
@@ -3,15 +3,26 @@ package com.college.questions;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class MainActivity extends AppCompatActivity {
+
+    int result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Button payBtn = findViewById(R.id.btnPayId);
+        Button rstBtn = findViewById(R.id.btnResetId);
+
+
 
         // This EditText contains the user input
         // for the number of burgers
@@ -27,5 +38,20 @@ public class MainActivity extends AppCompatActivity {
         // Result should contain 5 multiplied by number chosen
         // Display should be prefixed with the '$' sign such as $15
         // Reset should clear all the fields
+
+        payBtn.setOnClickListener(  ( click ) ->
+        {
+            int quantity = Integer.valueOf(numberStr.getText().toString());
+            result = quantity * 5;
+            resultTv.setText(String.valueOf("$" + result));
+
+        } );
+
+        rstBtn.setOnClickListener( (click) -> {
+            resultTv.setText("0");
+            numberStr.setText("");
+        });
+
+
     }
 }
